38e4b0b68144420a05f778089a4d59bf007fd064
Lucas Duarte <57960201+lucasduartedesigner@users.noreply.github.com>
2022-08-16 20:27:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,60 @@
+# Este é um exemplo para aula 2
+#
+# Lucas Duarte
 
+print("1 2 3")
+
+# Isso é uma string
+nome_var = "Boa noite..."
+
+# Escreve o tipo da variavel
+print(type(nome_var))
+
+print(nome_var)
+
+# Isso é uma string
+nome_var = "Oi turma..."
+
+# Isso é um float
+nome_var = 4.99
+
+# Escreve o tipo da variavel
+print(type(nome_var))
+
+print(nome_var)
+
+resultado = nome_var / 5
+
+print(resultado)
+
+# Isso é um int
+nome_var = 20
+
+print(type(nome_var))
+
+nome_var = True  # Isso é um booleano
+print(type(nome_var))
+
+print(" ")
+print(" ")
+
+#cria um input
+email = input('Digite seu email: ')
+
+print(email)
+print(type(email))
+
+produto = input('Digite o preço do produto: ')
+
+#Conversao de tipo
+preco = float(produto)
+
+print(preco)
+print(type(preco))
+
+# Este é um exemplo para aula 2
+nome   = input('Informe o nome:')
+idade  = int(input('Informe a idade:'))
+cidade = input('Informe a cidade:')
+
+print(f'O nome do usuário é {nome}. O usuário tem {idade} anos e mora em {cidade}')
